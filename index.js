aef0e7a41d5068b1b0711a7b2618a7e84f94ca8a
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-16 13:58:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(drivers, match){
   return drivers.filter(driver => driver.toLowerCase() === match.toLowerCase())
 }
+
+function fuzzyMatch(){
+  return driver.filter(driver => driver.slice(0,2).include(match))
+}
